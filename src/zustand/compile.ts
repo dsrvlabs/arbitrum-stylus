f9f0a0d6eabfbdcef40962b02248071ed894137e
Mark_Jeong <81726531+markjung96@users.noreply.github.com>
2024-10-07 15:47:28 +0900

    feat: add zustand compile

--- src/zustand/compile.ts
+++ src/zustand/compile.ts
@@ -1,9 +1,50 @@
 import { StateCreator } from "zustand";
 import { CompileState } from "./compile.types";
+import { produce } from "immer";
 
-export const createCompileStore: StateCreator<CompileState> = (set) => ({
+const initial = {
+  loading: false,
+  errorMsg: null,
   projectFiles: [],
   timestamp: 0,
-  setProjectFiles: (projectFiles: any) => set({ projectFiles }),
-  setTimeStamp: (timestamp: number) => set({ timestamp }),
+};
+
+export const createCompileStore: StateCreator<CompileState> = (set) => ({
+  compile: {
+    ...initial,
+    setProjectFiles: (projectFiles: any) =>
+      set(
+        produce((state: CompileState) => {
+          state.compile.projectFiles = projectFiles;
+        })
+      ),
+    setTimestamp: (timestamp: number) =>
+      set(
+        produce((state: CompileState) => {
+          state.compile.timestamp = timestamp;
+        })
+      ),
+    setErrorMsg: (msg: string | null) =>
+      set(
+        produce((state: CompileState) => {
+          state.compile.errorMsg = msg;
+        })
+      ),
+    setLoading: (loading: boolean) =>
+      set(
+        produce((state: CompileState) => {
+          state.compile.loading = loading;
+        })
+      ),
+    reset: () =>
+      set(
+        produce((state: CompileState) => {
+          state.compile = { ...state.compile, ...initial };
+        })
+      ),
+  },
+  // projectFiles: [],
+  // timestamp: 0,
+  // setProjectFiles: (projectFiles: any) => set({ projectFiles }),
+  // setTimeStamp: (timestamp: number) => set({ timestamp }),
 });
--- src/zustand/compile.types.ts
+++ src/zustand/compile.types.ts
@@ -1,6 +1,13 @@
 export interface CompileState {
-  projectFiles: any;
-  timestamp: number;
-  setProjectFiles: (projectFiles: any) => void;
-  setTimeStamp: (timestamp: number) => void;
+  compile: {
+    loading: boolean;
+    errorMsg: string | null;
+    projectFiles: any;
+    timestamp: number;
+    setProjectFiles: (projectFiles: any) => void;
+    setTimestamp: (timestamp: number) => void;
+    setErrorMsg: (msg: string | null) => void;
+    setLoading: (loading: boolean) => void;
+    reset: () => void;
+  };
 }
