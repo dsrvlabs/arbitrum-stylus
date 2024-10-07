f41e810f3116bd14e18804d46cc120c76e816501
Mark_Jeong <81726531+markjung96@users.noreply.github.com>
2024-10-07 15:41:23 +0900

    feat: add loader wrapper

--- src/components/common/loader.tsx
+++ src/components/common/loader.tsx
@@ -5,7 +5,7 @@ interface LoaderWrapperProps {
 }
 export const LoaderWrapper: React.FC<LoaderWrapperProps> = ({ loading }) => {
   return loading ? (
-    <div className="h-full w-full absolute flex items-center justify-center bg-gray-700 opacity-70">
+    <div className="h-full w-full absolute top-0 left-0 flex items-center justify-center bg-gray-700 opacity-70">
       <FaSpinner className="animate-spin" />
     </div>
   ) : null;
