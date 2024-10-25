06305c4e623b0c85aa14b77c09b13edd9c50a049
Mark_Jeong <81726531+markjung96@users.noreply.github.com>
2024-10-25 18:50:21 +0900

    fix: switch endpoint

--- src/const/endpoint.ts
+++ src/const/endpoint.ts
@@ -1,18 +1,18 @@
 import { STAGE } from "./stage";
 
 const COMPILER_API_ENDPOINT_POOL = {
-  // local: "http://localhost:8000",
+  local: "http://localhost:8000",
   // local: "https://cargo-test.veriwell.dev",
-  local: "https://arbitrum.compiler.welldonestudio.io",
+  // local: "https://arbitrum.compiler.welldonestudio.io",
   dev: "https://dev.compiler.welldonestudio.io",
-  prod: "https://verify.welldonestudio.io",
+  prod: "https://arbitrum.compiler.welldonestudio.io",
 };
 export const COMPILER_API_ENDPOINT = COMPILER_API_ENDPOINT_POOL[STAGE];
 
 const COMPILER_WEBSOCKET_ENDPOINT_POOL = {
-  // local: "ws://localhost:8000",
-  local: "wss://arbitrum.compiler.welldonestudio.io",
+  local: "ws://localhost:8000",
+  // local: "wss://arbitrum.compiler.welldonestudio.io",
   dev: "wss://dev.compiler.welldonestudio.io",
-  prod: "wss://prod.near.compiler.welldonestudio.io",
+  prod: "wss://arbitrum.compiler.welldonestudio.io",
 };
 export const COMPILER_WEBSOCKET_ENDPOINT = COMPILER_WEBSOCKET_ENDPOINT_POOL[STAGE];
